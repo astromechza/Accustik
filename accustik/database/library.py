b3b891be9aeb73b2e999f4c259e348d817502b24
Ben <benmeier42@gmail.com>
2013-01-24 21:58:15 +0200

    Fixed library.py

--- accustik/database/library.py
+++ accustik/database/library.py
@@ -1,8 +1,7 @@
-import sqlite3
 from accustik.logger import log
 import os
-from accustik.database.FTSDictionary import FTSDictionary
 from accustik.database.FTSSongData import FTSSongData
+from accustik.data.DataReaders import get_reader
 
 class Library:
     """
@@ -15,5 +14,24 @@ class Library:
         self.dbfile = dbfile
         self.songs = FTSSongData(self.dbfile, True)
 
-        
+    def add_file(self, path):
+        reader = get_reader(path)
+        sd = reader.read()
+        if sd is None:
+            log.debug('Unsupported filetype: ' + reader.file)
+        else:
+            self.songs.add(sd)
+
+    def add_folder(self, path):
+        self.songs.add_from_iterable(self._add_folder(path))
+
+    def _add_folder(self, path):
+        for f in os.listdir(path):
+            reader = get_reader(os.path.join(f))
+            sd = reader.read()
+
+            if sd is None:
+                log.debug('Unsupported filetype: ' + reader.file)
+            else:
+                yield sd
 
